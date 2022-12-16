862378d6d6f24485b946f04a028eaf6e2299bba9
Farzin Firouzi <farzineff@gmail.com>
2022-12-16 02:32:37 -0500

    18. Advanced - Django admin configuration

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from . models import Category, Product
 
-admin.site.register(Product)
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
