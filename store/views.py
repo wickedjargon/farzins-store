695c8178446d5d9b1b7fbb5f11e10267f9c12840
Farzin Firouzi <farzineff@gmail.com>
2022-12-18 07:28:09 -0500

    25. Building the category view

--- store/views.py
+++ store/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from . models import Category
 
 def store(request):
     return render(request, 'store/store.html')
+
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return {'all_categories':all_categories}
