40c50231c5254e1a56c4bd701b19526a0fa7a31d
Farzin Firouzi <farzineff@gmail.com>
2022-12-18 15:06:49 -0500

    34. Configure basic urls and views for our cart

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('store.urls')),
+    path('admin/', admin.site.urls), # admin url
+    path('', include('store.urls')), # store app
+    path('urls/', include('cart.urls')), # cart app
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
