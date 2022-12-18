66d184d46b9d915c5be96ef8a292a2ff25b0eaa8
Farzin Firouzi <farzineff@gmail.com>
2022-12-18 14:09:36 -0500

    31. Building the list category view

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.store, name='store'),
-    path('product/<slug:slug>/', views.product_info, name='product-info'),
+    path('', views.store, name='store'), # store main page
+    path('product/<slug:product_slug>/', views.product_info, name='product-info'), # individual product
+    path('search/<slug:category_slug>/', views.list_category, name='list-category'), # individual category
 ]
