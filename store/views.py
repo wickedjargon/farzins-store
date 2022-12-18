b8683b3653637b9beb70353e2ce041905579aad2
Farzin Firouzi <farzineff@gmail.com>
2022-12-18 07:46:16 -0500

    27. Build the main product data grid - Pt. 2

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from . models import Category
+from . models import Category, Product
 
 def store(request):
-    return render(request, 'store/store.html')
+    all_products = Product.objects.all()
+    context = {'my_products':all_products}
+    return render(request, 'store/store.html', context=context)
 
 
 def categories(request):
